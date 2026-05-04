ce139a5f09cdaf12fb1c9186c19c7cd33e8a2560
BudgetArms <67414205+BudgetArms@users.noreply.github.com>
2026-05-04 19:01:40 +0200

    Removed AudioQueue from ServiceLocator

--- Core/ServiceLocator.h
+++ Core/ServiceLocator.h
@@ -2,7 +2,6 @@
 
 #include <memory>
 
-#include "Sounds/AudioQueue.h"
 #include "Sounds/NullSoundSystem.h"
 #include "Sounds/SoundSystem.h"
 
@@ -12,17 +11,6 @@ namespace bae
     class ServiceLocator final
     {
     public:
-        static IAudioQueue& GetAudioQueue() { return *m_sAudioQueueInstance; }
-
-        template<typename AudioClipType,
-                 typename = std::enable_if_t<std::is_base_of_v<AudioClip, AudioClipType> &&
-                     !std::is_same_v<AudioClip, AudioClipType>>>
-        static void RegisterAudioQueue()
-        {
-            m_sAudioQueueInstance = std::make_unique<AudioQueue<AudioClipType>>();
-        }
-
-
         static SoundSystem& GetSoundSystem() { return *m_sSoundSystemInstance; }
 
         static void RegisterSoundSystem(std::unique_ptr<SoundSystem>&& soundSystem)
@@ -38,7 +26,6 @@ namespace bae
         }
 
     private:
-        static std::unique_ptr<IAudioQueue> m_sAudioQueueInstance;
         static std::unique_ptr<SoundSystem> m_sSoundSystemInstance;
     };
 }
