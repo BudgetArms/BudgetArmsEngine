6c4967665fd9609653dba5f975eaf7bdd1f29e49
BudgetArms <67414205+BudgetArms@users.noreply.github.com>
2026-05-25 19:29:19 +0200

    Replaced pragma warning with maybe_unused
    This was done to make the emscripten build work

--- Graphs/HeuristicFunctions.hpp
+++ Graphs/HeuristicFunctions.hpp
@@ -13,29 +13,26 @@
 
 namespace bae::Graphs::HeuristicFunctions
 {
-    #pragma warning(push)
-    #pragma warning(disable : 4505)
-
     // Manhattan distance
-    static float Manhattan(float x, float y)
+    [[maybe_unused]] static float Manhattan(float x, float y)
     {
         return static_cast<float>(x + y);
     }
 
     // Euclidean distance
-    static float Euclidean(float x, float y)
+    [[maybe_unused]] static float Euclidean(float x, float y)
     {
         return std::sqrtf(x * x + y * y);
     }
 
     // Sqrt Euclidean distance
-    static float SqEuclidean(float x, float y)
+    [[maybe_unused]] static float SqEuclidean(float x, float y)
     {
         return x * x + y * y;
     }
 
     // Octile distance
-    static float Octile(float x, float y)
+    [[maybe_unused]] static float Octile(float x, float y)
     {
         //const float f = 0.414213562373095048801f; // == sqrt(2) - 1;
         const float f = std::sqrtf(2) - 1; // std::sqrt is constexpr in C++26
@@ -43,12 +40,10 @@ namespace bae::Graphs::HeuristicFunctions
     }
 
     //Chebyshev distance
-    static float Chebyshev(float x, float y)
+    [[maybe_unused]] static float Chebyshev(float x, float y)
     {
         return std::max(x, y);
     }
-
-    #pragma warning(pop)
 }
 
 
